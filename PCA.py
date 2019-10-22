d0a540b840ae70f4d9e96aaa1a4b77db8bb712b1
shihe <engineer_it@126.com>
2019-10-22 17:13:04 +0800

    PCA

--- PCA.py
+++ PCA.py
@@ -1,5 +1,6 @@
 import numpy as np
 
+
 class PCA:
 
     def __init__(self, n_components):
@@ -9,9 +10,9 @@ class PCA:
         self.components_ = None
 
     def fit(self, X, eta=0.01, n_iters=1e4):
-        """获取数据集X的前n个主成分"""
+        """获得数据集X的前n个主成分"""
         assert self.n_components <= X.shape[1], \
-            "n_components must be greater than the feature number of X"
+            "n_components must not be greater than the feature number of X"
 
         def demean(X):
             return X - np.mean(X, axis=0)
@@ -19,10 +20,13 @@ class PCA:
         def f(w, X):
             return np.sum((X.dot(w) ** 2)) / len(X)
 
+        def df(w, X):
+            return X.T.dot(X.dot(w)) * 2. / len(X)
+
         def direction(w):
             return w / np.linalg.norm(w)
 
-        def first_component(X, initial_w, eta=0.01, epsilon=1e-8):
+        def first_component(X, initial_w, eta=0.01, n_iters=1e4, epsilon=1e-8):
 
             w = direction(initial_w)
             cur_iter = 0
@@ -39,17 +43,29 @@ class PCA:
 
             return w
 
-        def transform(self, X):
-            """将给定的X，映射到各主成分分量重"""
-            assert X.shape[1] == self.components_.shape[1]
+        X_pca = demean(X)
+        self.components_ = np.empty(shape=(self.n_components, X.shape[1]))
+        for i in range(self.n_components):
+            initial_w = np.random.random(X_pca.shape[1])
+            w = first_component(X_pca, initial_w, eta, n_iters)
+            self.components_[i,:] = w
 
-            return X.dot(self.components_.T)
+            X_pca = X_pca - X_pca.dot(w).reshape(-1, 1) * w
 
-        def inverse_transform(self, X):
-            """"""
-            assert X.shape[1] == self.components_.shape[0]
+        return self
 
-            return X.dot(self.components_)
+    def transform(self, X):
+        """将给定的X，映射到各个主成分分量中"""
+        assert X.shape[1] == self.components_.shape[1]
 
-        def __repr__(self):
-            return "PCA(n_components=%d)" % self.n_components
+        return X.dot(self.components_.T)
+
+    def inverse_transform(self, X):
+        """将给定的X，反向映射回原来的特征空间"""
+        assert X.shape[1] == self.components_.shape[0]
+
+        return X.dot(self.components_)
+
+    def __repr__(self):
+        return "PCA(n_components=%d)" % self.n_components
+
